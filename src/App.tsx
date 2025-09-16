3e46c9075c126a4ec9322e4364e7679d2b5b0bd3
[dyad] <spidro@seznam.cz>
2025-09-16 12:42:20 +0200

    [dyad] Added QueryClientProvider to App.tsx to resolve react-query errors. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { AuthProvider } from "@/context/AuthContext";
 import { Toaster } from "@/components/ui/sonner";
-import Layout from "@/components/Layout"; // Assuming you have a Layout component
+import Layout from "@/components/Layout";
 import Dashboard from "@/pages/Dashboard";
 import ConflictsPage from "@/pages/ConflictsPage";
 import Admin from "@/pages/Admin";
@@ -14,75 +14,79 @@ import NotFound from "@/pages/NotFound";
 import CountriesPage from "./pages/CountriesPage";
 import ViolationsPage from "./pages/ViolationsPage";
 import UNDeclarationsPage from "./pages/UNDeclarationsPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Import QueryClient and QueryClientProvider
 
+const queryClient = new QueryClient(); // Create a new QueryClient instance
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <AuthProvider>
-        <Router>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route
-              path="/"
-              element={
-                <Layout>
-                  <Dashboard />
-                </Layout>
-              }
-            />
-            <Route
-              path="/conflicts"
-              element={
-                <Layout>
-                  <ConflictsPage />
-                </Layout>
-              }
-            />
-            <Route
-              path="/admin"
-              element={
-                <Layout>
-                  <Admin />
-                </Layout>
-              }
-            />
-            <Route
-              path="/forum"
-              element={
-                <Layout>
-                  <Forum />
-                </Layout>
-              }
-            />
-            <Route
-              path="/countries"
-              element={
-                <Layout>
-                  <CountriesPage />
-                </Layout>
-              }
-            />
-            <Route
-              path="/violations"
-              element={
-                <Layout>
-                  <ViolationsPage />
-                </Layout>
-              }
-            />
-            <Route
-              path="/un-declarations"
-              element={
-                <Layout>
-                  <UNDeclarationsPage />
-                </Layout>
-              }
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
+        <QueryClientProvider client={queryClient}> {/* Wrap with QueryClientProvider */}
+          <Router>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route
+                path="/"
+                element={
+                  <Layout>
+                    <Dashboard />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/conflicts"
+                element={
+                  <Layout>
+                    <ConflictsPage />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/admin"
+                element={
+                  <Layout>
+                    <Admin />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/forum"
+                element={
+                  <Layout>
+                    <Forum />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/countries"
+                element={
+                  <Layout>
+                    <CountriesPage />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/violations"
+                element={
+                  <Layout>
+                    <ViolationsPage />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/un-declarations"
+                element={
+                  <Layout>
+                    <UNDeclarationsPage />
+                  </Layout>
+                }
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Router>
+        </QueryClientProvider>
         <Toaster />
       </AuthProvider>
     </ThemeProvider>
